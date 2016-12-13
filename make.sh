438fc37955b93c987542cbf1fd7c248dd888f3ad
Slyke <steven.lawler777@gmail.com>
2016-12-12 20:50:00 -0500

    Fixed up ordering of external headers

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@
  
 # rm wiscan
  
-g++ -lncursesw -lpthread cli.cpp wifilist.cpp wificell.cpp screens/scan.cpp main.cpp -o wiscan.so
+g++ cli.cpp wifilist.cpp wificell.cpp screens/scan.cpp main.cpp -o wiscan.so -lncursesw -lcurses -lncurses -lpthread 
